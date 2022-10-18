a58494a747874b92c783ec5bad560c14c6ce8709
Antonio Dell'Ava <antonio.dellava@gmail.com>
2022-10-18 13:01:34 +0200

    first block added

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,11 +1,18 @@
 import Header from "./Header";
+import style from "./Layout.module.scss";
 
 const Layout = ({ children }) => {
   return (
-    <>
-      <Header />
-      {children}
-    </>
+    <main className={style.Block}>
+      <header className={style.Block_headerContainer}>
+        <Header />
+      </header>
+      <section
+        className={`${style.Block_contentContainer} ${style.Block_contentContainer___compact}`}
+      >
+        {children}
+      </section>
+    </main>
   );
 };
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { Layout } from "../components/layout";
+import "./Document.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
