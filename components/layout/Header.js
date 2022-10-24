7a9e6b8cf7fbface8c2fcf4b022e3366b336f42a
Antonio Dell'Ava <antonio.dellava@gmail.com>
2022-10-24 12:02:54 +0200

    header added

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,8 +1,19 @@
+import style from "./Header.module.scss";
+import Link from "next/link";
+
 const Header = () => {
   return (
-    <nav>
-      <a href="/">Orders</a>
-    </nav>
+    <header className={style.Block}>
+      <h1 className={style.Block_title}>Orders App</h1>
+      <nav className={style.Block_navigation}>
+        <Link href="/">
+          <a className={style.Block_navigationItem}>Orders</a>
+        </Link>
+        <Link href="/create">
+          <a className={style.Block_navigationItem}>Create New Order</a>
+        </Link>
+      </nav>
+    </header>
   );
 };
 
