cadac2ed96430dade2c9aa538a8f9b305264e67a
Massimo Biagioli <biagiolimassimo@gmail.com>
2022-12-21 17:23:06 +0100

    Fix fonts

--- components/order/OrderCustomer.js
+++ components/order/OrderCustomer.js
@@ -3,11 +3,13 @@ import style from "./OrderCustomer.module.scss";
 const OrderCustomer = ({customer}) => {
     return (
         <div className={style.Block}>
-            <h3>CUSTOMER</h3>
-            <div>{customer.firstname} {customer.lastname}</div>
-            <div>{customer.email}</div>
+            <h3 className={style.Block_header}>CUSTOMER</h3>
+            <div className={style.Block_content}>
+                <div>{customer.firstname} {customer.lastname}</div>
+                <div>{customer.email}</div>
+            </div>
         </div>
     );
 };
 
-export default OrderCustomer;
+export default OrderCustomer
